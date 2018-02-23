feeff03d3497806802f6a29ebec05371b72790e7
RoyWP <36727602+RoyWP@users.noreply.github.com>
2018-02-23 12:39:53 +0200

    Add files via upload

---
+++ jsonESP8266/jsonESP8266.ino
@@ -0,0 +1,221 @@
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
+#include <hpma115S0.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiUdp.h>
+#include <ESP8266mDNS.h>
+
+#define ONE_HOUR 3600000UL //for unix time
+
+#define timeDelay   6000 // 6 second delay for response time of sensor
+
+long nextOperation = millis() + timeDelay; //
+
+SoftwareSerial hpmaSerial(12, 14, false, 128); // Feather TX, Feather RX
+
+HPMA115S0 hpma115S0(hpmaSerial); //set up instance of sensor structure
+
+ESP8266WiFiMulti wifiMulti;    // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+const char* mdnsName = "esp8266";        // Domain name for the mDNS responder
+
+WiFiUDP UDP;                   // Create an instance of the WiFiUDP class to send and receive UDP messages
+
+IPAddress timeServerIP;        // The time.nist.gov NTP server's IP address
+const char* ntpServerName = "time.nist.gov";
+
+const int NTP_PACKET_SIZE = 48;          // NTP time stamp is in the first 48 bytes of the message
+
+byte packetBuffer[NTP_PACKET_SIZE];      // A buffer to hold incoming and outgoing packets
+
+/*__________________________________________________________SETUP__________________________________________________________*/
+
+void setup() {
+  Serial.begin(57600);        // Start the serial communication to send messages to the computer
+  hpmaSerial.begin(9600);     // Establish serial comms to sensor
+  
+  
+
+  /*Some kind of reset function based on initialisation/open serial to make sure communication is working
+  //if (SOMETHING IS WRONG) {
+  //      ESP.reset();
+  }*/
+
+  startWiFi();                 // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+
+  startUDP();                  // Start listening for UDP messages to port 123
+
+  WiFi.hostByName(ntpServerName, timeServerIP); // Get the IP address of the NTP server
+  Serial.print("Time server IP:\t");
+  Serial.println(timeServerIP);
+  Serial.println("Starting measurements");
+  Serial.println("\r\n");
+  hpma115S0.Init();                     //initialise   
+  sendNTPpacket(timeServerIP);
+}
+
+/*__________________________________________________________LOOP__________________________________________________________*/
+
+const unsigned long intervalNTP = ONE_HOUR; // Update the time every hour
+unsigned long prevNTP = 0;
+unsigned long lastNTPResponse = millis();
+
+const unsigned long intervalPM = 60000;   // measure every minute
+unsigned long prevPM = 0;
+bool PMRequested = false;
+const unsigned long DS_delay = 6000;         // Response time of HMPA115S0 is <6s
+uint32_t timeUNIX = 0;                      // The most recent timestamp received from the time server
+
+void loop() {
+  unsigned long currentMillis = millis();
+  unsigned int pm2_5, pm10;   //variables to store and print device data
+
+  if (currentMillis - prevNTP > intervalNTP) { // Request the time from the time server every hour
+    prevNTP = currentMillis;
+    sendNTPpacket(timeServerIP);
+  }
+
+  uint32_t time = getTime();                   // Check if the time server has responded, if so, get the UNIX time
+  if (time) {
+    timeUNIX = time;
+    Serial.print("NTP response:\t");
+    Serial.println(timeUNIX);
+    lastNTPResponse = millis();
+  } else if ((millis() - lastNTPResponse) > 24UL * ONE_HOUR) {
+    Serial.println("More than 24 hours since last NTP response. Rebooting.");
+    Serial.flush();
+    ESP.reset();
+  }
+
+  if (timeUNIX != 0) {
+    if (currentMillis - prevPM > intervalPM) {  // Every minute, request the PM
+      hpma115S0.StartParticleMeasurement(); // Request the sensor to start sending measurements (it takes some time to read it)
+      PMRequested = true;
+      prevPM = currentMillis;
+      Serial.println("Request sent");
+    }
+      
+    if (currentMillis - prevPM > DS_delay && PMRequested) { // after delay
+      uint32_t actualTime = timeUNIX + (currentMillis - lastNTPResponse) / 1000;
+      // The actual time is the last NTP time plus the time that has elapsed since the last NTP response
+      PMRequested = false;
+      hpma115S0.ReadParticleMeasurement(&pm2_5,&pm10); // Get the PM from the sensor
+      hpma115S0.StopParticleMeasurement();
+
+      sendJSON(pm2_5,actualTime);   
+      
+    }
+  } else {                                    // If we didn't receive an NTP response yet, send another request
+    sendNTPpacket(timeServerIP);
+    delay(500);
+  }
+}
+
+/*__________________________________________________________SETUP_FUNCTIONS__________________________________________________________*/
+
+void startWiFi() { // Try to connect to some given access points. Then wait for a connection
+  wifiMulti.addAP("Roy", "Royhanna44");   // add Wi-Fi networks you want to connect to
+  wifiMulti.addAP("HAMKvisitor", "hamkvisitor");
+  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3"); //need Khoa's server's network!
+
+  Serial.println("Connecting");
+  while (wifiMulti.run() != WL_CONNECTED) {  // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println("\r\n");
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());             // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.print(WiFi.localIP());            // Send the IP address of the ESP8266 to the computer
+  Serial.println("\r\n");
+}
+
+void startUDP() {
+  Serial.println("Starting UDP");
+  UDP.begin(123);                          // Start listening for UDP messages to port 123
+  Serial.print("Local port:\t");
+  Serial.println(UDP.localPort());
+}
+
+void startMDNS() { // Start the mDNS responder
+  MDNS.begin(mdnsName);                        // start the multicast domain name server
+  Serial.print("mDNS responder started: http://");
+  Serial.print(mdnsName);
+  Serial.println(".local");
+}
+
+/*__________________________________________________________HELPER_FUNCTIONS__________________________________________________________*/
+
+String formatBytes(size_t bytes) { // convert sizes in bytes to KB and MB
+  if (bytes < 1024) {
+    return String(bytes) + "B";
+  } else if (bytes < (1024 * 1024)) {
+    return String(bytes / 1024.0) + "KB";
+  } else if (bytes < (1024 * 1024 * 1024)) {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  }
+}
+
+unsigned long getTime() { // Check if the time server has responded, if so, get the UNIX time, otherwise, return 0
+  if (UDP.parsePacket() == 0) { // If there's no response (yet)
+    return 0;
+  }
+  UDP.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+  // Combine the 4 timestamp bytes into one 32-bit number
+  uint32_t NTPTime = (packetBuffer[40] << 24) | (packetBuffer[41] << 16) | (packetBuffer[42] << 8) | packetBuffer[43];
+  // Convert NTP time to a UNIX timestamp:
+  // Unix time starts on Jan 1 1970. That's 2208988800 seconds in NTP time:
+  const uint32_t seventyYears = 2208988800UL;
+  // subtract seventy years:
+  uint32_t UNIXTime = NTPTime - seventyYears;
+  return UNIXTime;
+}
+
+
+void sendNTPpacket(IPAddress& address) {
+  Serial.println("Sending NTP request");
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);  // set all bytes in the buffer to 0
+  // Initialize values needed to form NTP request
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+
+  // send a packet requesting a timestamp:
+  UDP.beginPacket(address, 123); // NTP requests are to port 123
+  UDP.write(packetBuffer, NTP_PACKET_SIZE);
+  UDP.endPacket();
+}
+
+void sendJSON(uint8_t data,uint32_t tstamp){
+  StaticJsonBuffer<100> JSONbuffer;   //Declaring static JSON buffer
+  JsonObject& JSONencoder = JSONbuffer.createObject(); 
+  
+  JSONencoder["sensorType"] = "PM concentration";
+  
+  JsonArray& values = JSONencoder.createNestedArray("values"); //JSON array
+  values.add(data); //Add value to array
+  
+  JsonArray& timestamps = JSONencoder.createNestedArray("timestamps"); //JSON array
+  timestamps.add(tstamp); //Add value to array
+  
+  
+  
+  char JSONmessageBuffer[100];
+  JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+  Serial.println(JSONmessageBuffer);
+  
+  HTTPClient http;    //Declare object of class HTTPClient
+  
+  http.begin("KHOAS SERVER");      //Specify request destination
+  http.addHeader("Content-Type", "application/json");  //Specify content-type header
+  
+  int httpCode = http.POST(JSONmessageBuffer);   //Send the request
+  String payload = http.getString();                                        //Get the response payload
+  
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(payload);    //Print request response payload
+  
+  http.end();  //Close connection
+}
+
---
+++ webserv/webserver_on_board/webserver_on_board.ino
@@ -0,0 +1,298 @@
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
+#include <hpma115S0.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiUdp.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <FS.h>
+
+#define ONE_HOUR 3600000UL //for unix time
+
+#define timeDelay   6000 // 6 second delay for response time of sensor
+
+long nextOperation = millis() + timeDelay; //
+
+SoftwareSerial hpmaSerial(12, 14, false, 128); // Feather TX, Feather RX
+
+HPMA115S0 hpma115S0(hpmaSerial); //set up instance of sensor structure
+
+ESP8266WebServer server(80);       // create a web server on port 80
+
+File fsUploadFile;                                    // a File variable to temporarily store the received file
+
+ESP8266WiFiMulti wifiMulti;    // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+const char* mdnsName = "esp8266";        // Domain name for the mDNS responder
+
+WiFiUDP UDP;                   // Create an instance of the WiFiUDP class to send and receive UDP messages
+
+IPAddress timeServerIP;        // The time.nist.gov NTP server's IP address
+const char* ntpServerName = "time.nist.gov";
+
+const int NTP_PACKET_SIZE = 48;          // NTP time stamp is in the first 48 bytes of the message
+
+byte packetBuffer[NTP_PACKET_SIZE];      // A buffer to hold incoming and outgoing packets
+
+/*__________________________________________________________SETUP__________________________________________________________*/
+
+void setup() {
+  Serial.begin(57600);        // Start the serial communication to send messages to the computer
+  hpmaSerial.begin(9600);     // Establish serial comms to sensor
+  
+  
+
+  /*Some kind of reset function based on initialisation/open serial to make sure communication is working
+  //if (SOMETHING IS WRONG) {
+  //      ESP.reset();
+  }*/
+
+  startWiFi();                 // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+
+  startSPIFFS();               // Start the SPIFFS and list all contents
+
+  startMDNS();                 // Start the mDNS responder
+
+  startServer();               // Start a HTTP server with a file read handler and an upload handler
+
+  startUDP();                  // Start listening for UDP messages to port 123
+
+  WiFi.hostByName(ntpServerName, timeServerIP); // Get the IP address of the NTP server
+  Serial.print("Time server IP:\t");
+  Serial.println(timeServerIP);
+  Serial.println("Starting measurements");
+  Serial.println("\r\n");
+  hpma115S0.Init();                     //initialise   
+  sendNTPpacket(timeServerIP);
+}
+
+/*__________________________________________________________LOOP__________________________________________________________*/
+
+const unsigned long intervalNTP = ONE_HOUR; // Update the time every hour
+unsigned long prevNTP = 0;
+unsigned long lastNTPResponse = millis();
+
+const unsigned long intervalPM = 60000;   // measure every minute
+unsigned long prevPM = 0;
+bool PMRequested = false;
+const unsigned long DS_delay = 6000;         // Response time of HMPA115S0 is <6s
+uint32_t timeUNIX = 0;                      // The most recent timestamp received from the time server
+
+void loop() {
+  unsigned long currentMillis = millis();
+  unsigned int pm2_5, pm10;   //variables to store and print device data
+
+  if (currentMillis - prevNTP > intervalNTP) { // Request the time from the time server every hour
+    prevNTP = currentMillis;
+    sendNTPpacket(timeServerIP);
+  }
+
+  uint32_t time = getTime();                   // Check if the time server has responded, if so, get the UNIX time
+  if (time) {
+    timeUNIX = time;
+    Serial.print("NTP response:\t");
+    Serial.println(timeUNIX);
+    lastNTPResponse = millis();
+  } else if ((millis() - lastNTPResponse) > 24UL * ONE_HOUR) {
+    Serial.println("More than 24 hours since last NTP response. Rebooting.");
+    Serial.flush();
+    ESP.reset();
+  }
+
+  if (timeUNIX != 0) {
+    if (currentMillis - prevPM > intervalPM) {  // Every minute, request the PM
+      hpma115S0.StartParticleMeasurement(); // Request the sensor to start sending measurements (it takes some time to read it)
+      PMRequested = true;
+      prevPM = currentMillis;
+      Serial.println("Request sent");
+    }
+      
+    if (currentMillis - prevPM > DS_delay && PMRequested) { // after delay
+      uint32_t actualTime = timeUNIX + (currentMillis - lastNTPResponse) / 1000;
+      // The actual time is the last NTP time plus the time that has elapsed since the last NTP response
+      PMRequested = false;
+      hpma115S0.ReadParticleMeasurement(&pm2_5,&pm10); // Get the PM from the sensor
+      hpma115S0.StopParticleMeasurement();
+      
+      Serial.printf("Appending data to file: %lu,", actualTime);
+      Serial.println(pm2_5);
+      File PMLog = SPIFFS.open("/PM.csv", "a"); // Write the time and the data to the csv file
+      PMLog.print(actualTime);
+      PMLog.print(',');
+      PMLog.println(pm2_5);
+      PMLog.close();
+    }
+  } else {                                    // If we didn't receive an NTP response yet, send another request
+    sendNTPpacket(timeServerIP);
+    delay(500);
+  }
+
+  server.handleClient();                      // run the server
+}
+
+/*__________________________________________________________SETUP_FUNCTIONS__________________________________________________________*/
+
+void startWiFi() { // Try to connect to some given access points. Then wait for a connection
+  wifiMulti.addAP("Roy", "Royhanna44");   // add Wi-Fi networks you want to connect to
+  wifiMulti.addAP("HAMKvisitor", "hamkvisitor");
+  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3"); //need Khoa's server's network!
+
+  Serial.println("Connecting");
+  while (wifiMulti.run() != WL_CONNECTED) {  // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println("\r\n");
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());             // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.print(WiFi.localIP());            // Send the IP address of the ESP8266 to the computer
+  Serial.println("\r\n");
+}
+
+void startUDP() {
+  Serial.println("Starting UDP");
+  UDP.begin(123);                          // Start listening for UDP messages to port 123
+  Serial.print("Local port:\t");
+  Serial.println(UDP.localPort());
+}
+
+
+void startSPIFFS() { // Start the SPIFFS and list all contents
+  SPIFFS.begin();                             // Start the SPI Flash File System (SPIFFS)
+  Serial.println("SPIFFS started. Contents:");
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {                      // List the file system contents
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      Serial.printf("\tFS File: %s, size: %s\r\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    Serial.printf("\n");
+  }
+}
+
+void startMDNS() { // Start the mDNS responder
+  MDNS.begin(mdnsName);                        // start the multicast domain name server
+  Serial.print("mDNS responder started: http://");
+  Serial.print(mdnsName);
+  Serial.println(".local");
+}
+
+void startServer() { // Start a HTTP server with a file read handler and an upload handler
+  server.on("/edit.html",  HTTP_POST, []() {  // If a POST request is sent to the /edit.html address,
+    server.send(200, "text/plain", "");
+  }, handleFileUpload);                       // go to 'handleFileUpload'
+
+  server.onNotFound(handleNotFound);          // if someone requests any other file or page, go to function 'handleNotFound'
+  // and check if the file exists
+
+  server.begin();                             // start the HTTP server
+  Serial.println("HTTP server started.");
+}
+
+/*__________________________________________________________SERVER_HANDLERS__________________________________________________________*/
+
+void handleNotFound() { // if the requested file or page doesn't exist, return a 404 not found error
+  if (!handleFileRead(server.uri())) {        // check if the file exists in the flash memory (SPIFFS), if so, send it
+    server.send(404, "text/plain", "404: File Not Found");
+  }
+}
+
+bool handleFileRead(String path) { // send the right file to the client (if it exists)
+  Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "index.html";          // If a folder is requested, send the index file
+  String contentType = getContentType(path);             // Get the MIME type
+  String pathWithGz = path + ".gz";
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) { // If the file exists, either as a compressed archive, or normal
+    if (SPIFFS.exists(pathWithGz))                         // If there's a compressed version available
+      path += ".gz";                                         // Use the compressed verion
+    File file = SPIFFS.open(path, "r");                    // Open the file
+    size_t sent = server.streamFile(file, contentType);    // Send it to the client
+    file.close();                                          // Close the file again
+    Serial.println(String("\tSent file: ") + path);
+    return true;
+  }
+  Serial.println(String("\tFile Not Found: ") + path);   // If the file doesn't exist, return false
+  return false;
+}
+
+void handleFileUpload() { // upload a new file to the SPIFFS
+  HTTPUpload& upload = server.upload();
+  String path;
+  if (upload.status == UPLOAD_FILE_START) {
+    path = upload.filename;
+    if (!path.startsWith("/")) path = "/" + path;
+    if (!path.endsWith(".gz")) {                         // The file server always prefers a compressed version of a file
+      String pathWithGz = path + ".gz";                  // So if an uploaded file is not compressed, the existing compressed
+      if (SPIFFS.exists(pathWithGz))                     // version of that file must be deleted (if it exists)
+        SPIFFS.remove(pathWithGz);
+    }
+    Serial.print("handleFileUpload Name: "); Serial.println(path);
+    fsUploadFile = SPIFFS.open(path, "w");               // Open the file for writing in SPIFFS (create if it doesn't exist)
+    path = String();
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    if (fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize); // Write the received bytes to the file
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (fsUploadFile) {                                   // If the file was successfully created
+      fsUploadFile.close();                               // Close the file again
+      Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
+      server.sendHeader("Location", "/success.html");     // Redirect the client to the success page
+      server.send(303);
+    } else {
+      server.send(500, "text/plain", "500: couldn't create file");
+    }
+  }
+}
+
+/*__________________________________________________________HELPER_FUNCTIONS__________________________________________________________*/
+
+String formatBytes(size_t bytes) { // convert sizes in bytes to KB and MB
+  if (bytes < 1024) {
+    return String(bytes) + "B";
+  } else if (bytes < (1024 * 1024)) {
+    return String(bytes / 1024.0) + "KB";
+  } else if (bytes < (1024 * 1024 * 1024)) {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  }
+}
+
+String getContentType(String filename) { // determine the filetype of a given filename, based on the extension
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+unsigned long getTime() { // Check if the time server has responded, if so, get the UNIX time, otherwise, return 0
+  if (UDP.parsePacket() == 0) { // If there's no response (yet)
+    return 0;
+  }
+  UDP.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+  // Combine the 4 timestamp bytes into one 32-bit number
+  uint32_t NTPTime = (packetBuffer[40] << 24) | (packetBuffer[41] << 16) | (packetBuffer[42] << 8) | packetBuffer[43];
+  // Convert NTP time to a UNIX timestamp:
+  // Unix time starts on Jan 1 1970. That's 2208988800 seconds in NTP time:
+  const uint32_t seventyYears = 2208988800UL;
+  // subtract seventy years:
+  uint32_t UNIXTime = NTPTime - seventyYears;
+  return UNIXTime;
+}
+
+
+void sendNTPpacket(IPAddress& address) {
+  Serial.println("Sending NTP request");
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);  // set all bytes in the buffer to 0
+  // Initialize values needed to form NTP request
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+
+  // send a packet requesting a timestamp:
+  UDP.beginPacket(address, 123); // NTP requests are to port 123
+  UDP.write(packetBuffer, NTP_PACKET_SIZE);
+  UDP.endPacket();
+}
+
